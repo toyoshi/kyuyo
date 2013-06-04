8bbb248f60920269a963e897a603547c032171fb
toyoshi <toyoshi@standfirm.jp>
2013-06-04 10:28:20 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kyuyo::Application.config.secret_token = '7a1309b53806d7f765199ef9e83ca6402b5ace75c4265ff1de7202eefccd9f9c419971430625927e223f6498f63e541eacee7ffc097c0cae18113160ecaf6a68'
