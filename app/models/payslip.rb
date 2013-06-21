3d6445ea812c0cef6bb077ab2f5aa9604521be9a
hidakatsuya <work.hidaka@gmail.com>
2013-06-21 12:49:10 +0900

    refactor Payslip#to_pdf
    page#values(Hash) で値を一括で設定することが出来るため、
    Payslip のカラム名と値を #attributes で取得し、テンプレートに
    存在するカラムのみの Hash をそのまま渡すように変更。
    
    また、#strftime で日付の書式を設定していたところも
    Editor の日付時刻書式にて設定し、金額についても
    Editor の基本書式を使うよう変更。

--- app/models/payslip.rb
+++ app/models/payslip.rb
@@ -17,28 +17,12 @@ class Payslip < ActiveRecord::Base
 
   def to_pdf
     reports = ThinReports::Report.new layout: File.join(Rails.root , 'app', 'reports', 'basic')
+
     reports.start_new_page do |page|
-      page.item(:name).value(self.name)
-
-      begin
-        page.item(:payslip_date).value(self.payslip_date.strftime("%Y年%m月"))
-      rescue
-      end
-
-      %w(work_from work_to).each do |key|
-        begin
-        page.item(key.to_sym).value(self.send(key).strftime("%Y年%m月%d日"))
-        rescue
-        end
-      end
-
-      %w(working_days working_time outworking_time allow_1_label allow_2_label allow_3_label allow_4_label allow_5_label deduction_1_label deduction_2_label deduction_3_label).each do |key|
-        page.item(key.to_sym).value(self.send(key))
-      end
-
-      %w(basic_pay transportation_expenses income_tax allowanance_amount deduction_amount balance allow_1 allow_2 allow_3 allow_4 allow_5 deduction_1 deduction_2 deduction_3 unemployment_insurance welfare_pension health_insurance residents_tax).each do |key|
-        page.item(key.to_sym).value("#{number_with_delimiter(self.send(key))}円")
-      end
+      page.values(self.attributes.select {|k, v| page.item_exists?(k) })
+      page.values(allowanance_amount: allowanance_amount, 
+                  deduction_amount: deduction_amount, 
+                  balance: balance)
     end
     reports.generate
   end
